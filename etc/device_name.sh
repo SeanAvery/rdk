9f5b6cb01a2e3366fdd0f60c3a97ea2335532141
seanavery <seanpollock3344@gmail.com>
2023-03-02 17:13:16 -0500

    Handle Jetson parse for nano base model

--- etc/device_name.sh
+++ etc/device_name.sh
@@ -3,8 +3,20 @@
 set -e
 
 if [ "$(uname)" == "Linux" ]; then
-    model=$(awk '{print $1}' /sys/firmware/devicetree/base/model)
-    echo $model
+    model=$(cat /sys/firmware/devicetree/base/model | tr -d '\0')
+
+    read -a arr <<< "$model"
+    for i in "${arr[@]}"
+    do
+        if [ "$i" == "Raspberry" ]; then
+            echo "Raspberry"
+            break
+
+        elif [ "$i" == "Jetson" ]; then
+            echo "Jetson"
+            break
+        fi
+    done
 fi
 
 exit 0
